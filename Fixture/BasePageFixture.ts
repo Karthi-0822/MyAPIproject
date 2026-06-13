2e38b8f83e13ed0587722e27c63716e1846bc1d4
Karthik Avula <karthikavula0829@gmail.com>
2026-06-13 22:12:58 +0530

    Logging the BaseURL

--- Fixture/BasePageFixture.ts
+++ Fixture/BasePageFixture.ts
@@ -6,8 +6,13 @@ type MyFixtures = {
 };
 
 export const test = base.extend<MyFixtures>({
-  petStore: async ({ request, baseURL }, use) => {
-    console.log('baseURL:', baseURL);
+  petStore: async ({ request }, use) => {
+    const baseURL = process.env.BASE_URL;
+    if (!baseURL) {
+    throw new Error(
+      'BASE_URL environment variable is missing'
+    );
+  }
     await use(new PetStoreService(request, baseURL!));
   }
 })
--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    baseURL: process.env.BaseURL,
+    baseURL: process.env.BASE_URL,
     trace: 'on-first-retry',
   },
 
